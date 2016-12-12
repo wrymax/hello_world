53c119a9dbf2c9cc967c220fe3dcdaa0ce2d40f5
王若愚 <wrymax@qq.com>
2016-12-12 23:33:36 +0800

    router

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  match '/webhook', to: "webhook#process", as: :process_webhook
+  match 'webhook', to: "webhook#process", via: [:get, :post]
 end
